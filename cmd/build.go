2457ba163c119eea4ced85609521e9c87c2fb1e3
mleader <mleader@redhat.com>
2022-10-15 14:33:15 -0400

    handle docker client push and build errors

--- cmd/build.go
+++ cmd/build.go
@@ -111,7 +111,10 @@ var buildCmd = &cobra.Command{
 	},
 }
 
-func BuildCmdMain(build_img bool, push_img bool, cec ce_client.ContainerEngineClient, conf config, abspath string, filenames []string, logger log.Logger, pythonCodeStyleChecker func(abspath string, stdout *bytes.Buffer, logger log.Logger) error) (bool, error) {
+func BuildCmdMain(build_img bool, push_img bool, cec ce_client.ContainerEngineClient, conf config, abspath string,
+	filenames []string, logger log.Logger,
+	pythonCodeStyleChecker func(abspath string, stdout *bytes.Buffer, logger log.Logger) error) (bool, error) {
+
 	meets_reqs := make([]bool, 3)
 	basic_reqs, err := BasicRequirements(filenames, logger)
 	if err != nil {
@@ -146,29 +149,33 @@ func BuildCmdMain(build_img bool, push_img bool, cec ce_client.ContainerEngineCl
 	return true, nil
 }
 
-func BuildImage(build_img bool, all_checks bool, cec ce_client.ContainerEngineClient, abspath string, image_name string, image_tag string, logger log.Logger) error {
+func BuildImage(build_img bool, all_checks bool, cec ce_client.ContainerEngineClient, abspath string, image_name string,
+	image_tag string, logger log.Logger) error {
+
 	if all_checks && build_img {
 		logger.Infof("Passed all requirements: %s %s\n", image_name, image_tag)
 		logger.Infof("Building %s %s from %v\n", image_name, image_tag, abspath)
-		if err := cec.Build(abspath, image_name, []string{image_tag}, logger); err != nil {
+		if err := cec.Build(abspath, image_name, []string{image_tag}); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func PushImage(all_checks, build_image, push_image bool, cec ce_client.ContainerEngineClient, name, version, username, password, registry_address, registry_namespace string, logger log.Logger) error {
+func PushImage(all_checks, build_image, push_image bool, cec ce_client.ContainerEngineClient, name, version,
+	username, password, registry_address, registry_namespace string, logger log.Logger) error {
+
 	if all_checks && build_image && push_image {
 		image_name_tag := name + ":" + version
 		destination := strings.Join(
 			[]string{registry_address, registry_namespace, image_name_tag},
 			"/")
 		logger.Infof("Pushing %s to %s", name, destination)
-		err := cec.Tag(image_name_tag, destination, logger)
+		err := cec.Tag(image_name_tag, destination)
 		if err != nil {
 			return err
 		}
-		err = cec.Push(destination, username, password, registry_address, logger)
+		err = cec.Push(destination, username, password, registry_address)
 		if err != nil {
 			return err
 		}
