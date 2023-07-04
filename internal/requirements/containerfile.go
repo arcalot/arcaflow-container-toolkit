35caf89234f08580966a0f2f983629231d4f48c5
Dustin Black <dblack@redhat.com>
2023-07-04 12:17:08 +0200

    update supported container base image (#63)
    * update supported container base image
    
    * syntax fix
    
    * gofmt

--- internal/requirements/containerfile.go
+++ internal/requirements/containerfile.go
@@ -37,9 +37,9 @@ func ContainerfileRequirements(abspath string, logger log.Logger) (bool, error) 
 
 		// create map of regexp patterns to search for in Dockerfile as well as log information if not found
 		m := map[string]string{
-			"FROM quay\\.io/centos/centos:stream8":                                             "Dockerfile doesn't use 'FROM quay.io/centos/centos:stream8'\n",
-			"(ADD|COPY) .*/?LICENSE /.*":                                                       "Dockerfile does not contain copy of arcaflow plugin license\n",
-			"CMD \\[\\]":                                                                       "Dockerfile does not contain an empty command (i.e. CMD [])",
+			"FROM quay\\.io/(centos/centos:stream8|arcalot/arcaflow-plugin-baseimage-python-.*base$)": "Dockerfile doesn't use a supported base image\n",
+			"(ADD|COPY) .*/?LICENSE /.*": "Dockerfile does not contain copy of arcaflow plugin license\n",
+			"CMD \\[\\]":                 "Dockerfile does not contain an empty command (i.e. CMD [])",
 			"LABEL org.opencontainers.image.source=\".*\"":                                     "Dockerfile is missing LABEL org.opencontainers.image.source",
 			"LABEL org.opencontainers.image.licenses=\"Apache-2\\.0.*\"":                       "Dockerfile is missing LABEL org.opencontainers.image.licenses",
 			"LABEL org.opencontainers.image.vendor=\"Arcalot project\"":                        "Dockerfile is missing LABEL org.opencontainers.image.vendor",
